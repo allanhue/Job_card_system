41071c099ae1f81b7ae43d6be569f3115c8e0e00
allanhue <allanmwangi329@gmail.com>
2026-02-04 22:34:10 +0300

    debug auth token

--- back/main.py
+++ back/main.py
@@ -12,8 +12,6 @@ app.add_middleware(
         "https://jobcardsystem-zeta.vercel.app",
         "http://localhost:3000",
         "http://localhost:3001",
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:3001"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
